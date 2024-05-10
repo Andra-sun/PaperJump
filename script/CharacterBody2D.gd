628eed746b9cb4b0cf6036054ad22fee95451d0c
Andra <camileguimaraes463@gmail.com>
2024-05-10 15:46:27 -0300

    b

--- script/CharacterBody2D.gd
+++ script/CharacterBody2D.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -500.0
+const SPEED = 800.0
+const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready
+var sprite = $Sprite2D
 
 
 func _physics_process(delta):
@@ -22,6 +24,7 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
